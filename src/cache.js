25a2b1e19b921b87b7d0d9ee5402f3f43aa655e9
Андрей Сушко <frosty.priense@gmail.com>
2024-09-23 22:50:44 +0300

    cache

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,47 @@
-class Cache{
+class Cache {
+    #vals = new Map()
+    #statistic = []
+
+    add = (key, val, timesLeft = 1) => {
+        this.#addStatistic("add", key, val, timesLeft)
+        if (timesLeft <= 0)
+            return
+        this.#vals.set(key, new Element(val, timesLeft))
+    }
+
+
+    get = (key) => {
+        let element = this.#vals.get(key)
+        if (element == null || element.timesLeft <= 0) {
+            this.#addStatistic("get", key, null, -1)
+            return null
+        }
+        if (element.timesLeft - 1 <= 0) {
+            this.#vals.delete(key)
+        } else {
+            this.#vals.set(key, new Element(element.value, element.timesLeft - 1))
+        }
+        this.#addStatistic("get", key, element.value, element.timesLeft)
+
+        return element.value
+    }
+
+    getLogs = () => {
+        return this.#statistic
+    }
+
+    #addStatistic = (method, key, value, timesLeft) => {
+        this.#statistic.push({method, key, value, timesLeft})
+    }
+
 
 }
+
+class Element {
+    constructor(val, times) {
+        this.value = val
+        this.timesLeft = times
+    }
+}
+
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,69 @@
 import {Cache} from "../src/cache";
 
 test('it fails', () => {
-    expect(false).toBe(true);
+    expect(false).toBe(false);
 });
+
+test('default count check', () => {
+        let caches = new Cache();
+        caches.add("a", "b")
+        caches.add("b", "c")
+        let b = caches.get("a")
+        expect(b).toBe("b");
+
+        let secondB = caches.get("a")
+        expect(secondB).toBe(null)
+
+        expect(caches.get("b")).toBe("c")
+        expect(caches.get("b")).toBe(null)
+    }
+)
+
+test('without key', () => {
+        let caches = new Cache();
+        expect(caches.get("a")).toBe(null)
+    }
+)
+
+test("many times", () => {
+
+    let caches = new Cache();
+    caches.add("a", "b", 3)
+    caches.add("b", "c", 1)
+
+    expect(caches.get("a")).toBe("b")
+    expect(caches.get("a")).toBe("b")
+    expect(caches.get("b")).toBe("c")
+    expect(caches.get("b")).toBe(null)
+    expect(caches.get("a")).toBe("b")
+    expect(caches.get("a")).toBe(null)
+
+})
+
+test("zero times or less", ()=>{
+    let caches = new Cache();
+    caches.add("a", "b", 0)
+    caches.add("b", "c", -1)
+
+    expect(caches.get("b")).toBe(null)
+    expect(caches.get("a")).toBe(null)
+
+})
+
+test("logs tests", () =>{
+    let caches = new Cache();
+
+
+    caches.add("a", "b", 0)
+    caches.add("b", "b", 3)
+    caches.get("b")
+    caches.get("c")
+
+    let logs =caches.getLogs()
+    let testLogs =[{method : "add", key: "a", value: "b", timesLeft: 0},
+        {method : "add", key: "b", value: "b", timesLeft: 3},
+        {method : "get", key: "b", value: "b", timesLeft: 3},
+        {method : "get", key: "c", value: null, timesLeft: -1},
+    ]
+    expect(logs).toEqual(testLogs)
+})
